ba3ab618cec5c369655e83df77ff0127ca49a32b
Santidvb <santiagobenjamindv@gmail.com>
2026-02-16 19:00:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const intro = document.getElementById("intro");
 const final = document.getElementById("final");
 const bgm = document.getElementById("bgm");
 
-boton.addEventListener("click", async () => {
+boton.addEventListener("pointerdown", () => {
+   bgm.volume = 0.7;
+   bgm.play().catch(e => alert("no suena bro" + e.name));
+	   
+
 	intro.classList.add("hidden");
 	final.classList.remove("hidden");
 	
 	document.body.classList.remove("MA");
 	document.body.classList.add("MI");
  
-   bgm.play()
 });
