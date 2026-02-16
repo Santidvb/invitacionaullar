2bf5d87d1ef82398601896d21515d29199f64cd7
Santidvb <santiagobenjamindv@gmail.com>
2026-02-16 20:37:59 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,31 +4,23 @@ const final = document.getElementById("final");
 const bgm = document.getElementById("bgm");
 
 function abrirInvitacion(e) {
-	e.preventDefault();
 	
-	bgm.muted = true;
-	const p = bgm.play();
-	if (p) 
-	p.then(() => {
-		bgm.pause();
-		bgm.currentTime = 0;
-		bgm.muted = false;
-		
-	    bgm.play().catch(err => alert("No funciona" + err.name));
-	    
-	}).catch(err => alert("bloqueado" + err.name));
-
+	bgm.currentTime = 0;
+	bgm.muted = false;
 	
-	
-	
-	
-	
+	bgm.play()
+	   .then(() => {
+		   
 	intro.classList.add("hidden");
 	final.classList.remove("hidden");
 	
 	document.body.classList.remove("MA");
 	document.body.classList.add("MI");
+})
+.catch(err => {
+	console.log("Error: " + err);
+	alert("Toca de nuevo bro");
+});
 }
 
-abrir.addEventListener("touchstart", abrirInvitacion, {passive: true});
 abrir.addEventListener("click", abrirInvitacion);
